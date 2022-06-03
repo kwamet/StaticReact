674a2aac66bb20c3c1362b63783047cc7cecba23
Kwame Trancoso <kwametrancoso@Kwames-MacBook-Air.local>
2022-06-03 16:43:10 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <img src="../logo192.png"className="nav--logo"></img>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    );
+}
+
+export default Navbar
